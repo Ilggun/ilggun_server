1c43bf26ab6786c85078078c7e0a83adaa8885c4
Dev-Beom <rtw_master@naver.com>
2021-06-02 02:28:11 +0900

    Update Controllers with API Impliciparams
    JWT 인증방식 추가로 인한,
    헤더(X-Auth-Token)에 들어갈 정보를 명시햇습니다.

--- src/main/java/com/ilggun/backend/product/controller/ProductController.java
+++ src/main/java/com/ilggun/backend/product/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.ilggun.backend.product.controller;
 
+import com.ilggun.backend.product.dto.ProductListResponseDto;
 import com.ilggun.backend.product.dto.ProductSaveRequestDto;
 import com.ilggun.backend.product.domain.Product;
 import com.ilggun.backend.product.dto.ProductUpdateRequestDto;
@@ -7,9 +8,7 @@ import com.ilggun.backend.product.service.ProductService;
 import com.ilggun.backend.response.CommonResult;
 import com.ilggun.backend.response.ListResult;
 import com.ilggun.backend.response.SingleResult;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +20,14 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
     private final ProductService productService;
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "X-AUTH-TOKEN",
+                    value = "로그인 성공 후 access_token",
+                    required = true,
+                    dataType = "String",
+                    paramType = "header")
+    })
     @ApiOperation(value = "제품 등록", notes = "제품을 등록합니다.")
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
@@ -28,13 +35,29 @@ public class ProductController {
         return productService.save(requestDto);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "X-AUTH-TOKEN",
+                    value = "로그인 성공 후 access_token",
+                    required = true,
+                    dataType = "String",
+                    paramType = "header")
+    })
     @ApiOperation(value = "모든 제품 검색", notes = "모든 제품을 검색합니다.")
     @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
-    public ListResult<Product> findAllProduct() {
+    public ListResult<ProductListResponseDto> findAllProduct() {
         return productService.findAllProduct();
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "X-AUTH-TOKEN",
+                    value = "로그인 성공 후 access_token",
+                    required = true,
+                    dataType = "String",
+                    paramType = "header")
+    })
     @ApiOperation(value = "제품 id로 제품 정보 검색", notes = "제품의 id로 제품의 정보를 검색합니다.")
     @GetMapping("/id/{id}")
     @ResponseStatus(HttpStatus.OK)
@@ -42,13 +65,29 @@ public class ProductController {
         return productService.findByProductId(id);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "X-AUTH-TOKEN",
+                    value = "로그인 성공 후 access_token",
+                    required = true,
+                    dataType = "String",
+                    paramType = "header")
+    })
     @ApiOperation(value = "사용자 보유 제품 정보 검색", notes = "사용자의 id로 사용자가 보유하고 있는 제품들의 정보를 출력합니다.")
     @GetMapping("/user/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public ListResult<Product> findByUserId(@ApiParam(value = "사용자의 고유 id", required = true) @PathVariable Long id) {
+    public ListResult<ProductListResponseDto> findByUserId(@ApiParam(value = "사용자의 고유 id", required = true) @PathVariable Long id) {
         return productService.findByUserId(id);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "X-AUTH-TOKEN",
+                    value = "로그인 성공 후 access_token",
+                    required = true,
+                    dataType = "String",
+                    paramType = "header")
+    })
     @ApiOperation(value = "제품 id로 제품 정보 변경", notes = "제품의 정보를 변경합니다.")
     @PutMapping("/id/{id}")
     @ResponseStatus(HttpStatus.OK)
@@ -56,6 +95,14 @@ public class ProductController {
         return productService.update(id, requestDto);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "X-AUTH-TOKEN",
+                    value = "로그인 성공 후 access_token",
+                    required = true,
+                    dataType = "String",
+                    paramType = "header")
+    })
     @ApiOperation(value = "제품 id로 제품 정보 삭제", notes = "제품의 정보를 삭제합니다.")
     @DeleteMapping("/id/{id}")
     @ResponseStatus(HttpStatus.OK)
--- src/main/java/com/ilggun/backend/user/controller/UserController.java
+++ src/main/java/com/ilggun/backend/user/controller/UserController.java
@@ -2,16 +2,14 @@ package com.ilggun.backend.user.controller;
 
 import com.ilggun.backend.response.CommonResult;
 import com.ilggun.backend.response.ListResult;
-import com.ilggun.backend.response.ResponseService;
 import com.ilggun.backend.response.SingleResult;
 import com.ilggun.backend.user.domain.User;
-import com.ilggun.backend.user.dto.UserSaveRequestDto;
 import com.ilggun.backend.user.service.UserService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @Api(tags = {"1. User"})
@@ -21,13 +19,46 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
-    @ApiOperation(value = "회원 가입", notes = "회원 가입입니다.")
-    @PostMapping("")
-    @ResponseStatus(HttpStatus.CREATED)
-    public SingleResult<User> save(@RequestBody UserSaveRequestDto requestDto) {
-        return userService.save(requestDto);
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "X-AUTH-TOKEN",
+                    value = "로그인 성공 후 access_token",
+                    required = true,
+                    dataType = "String",
+                    paramType = "header")
+    })
+    @ApiOperation(value = "회원 리스트 조회", notes = "모든 회원의 정보를 출력합니다.")
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public ListResult<User> findAllUser() {
+        return userService.findAllUser();
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "X-AUTH-TOKEN",
+                    value = "로그인 성공 후 access_token",
+                    required = true,
+                    dataType = "String",
+                    paramType = "header")
+    })
+    @ApiOperation(value = "개별 회원 정보 조회", notes = "개별 회원의 정보를 조회합니다.")
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public SingleResult<User> findUserById() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String id = authentication.getName();
+        return userService.findByUsername(id);
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(
+                    name = "X-AUTH-TOKEN",
+                    value = "로그인 성공 후 access_token",
+                    required = true,
+                    dataType = "String",
+                    paramType = "header")
+    })
     @ApiOperation(value = "회원 계정 삭제", notes = "회원의 계정을 삭제합니다.")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
@@ -35,18 +66,4 @@ public class UserController {
         return userService.delete(id);
     }
 
-    @ApiOperation(value = "모든 회원 정보", notes = "모든 회원의 정보를 출력합니다.")
-    @GetMapping("")
-    @ResponseStatus(HttpStatus.OK)
-    public ListResult<User> findAllUser() {
-        return userService.findAllUser();
-    }
-
-    @ApiOperation(value = "개별 회원 정보 조회", notes = "개별 회원의 정보를 조회합니다.")
-    @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public SingleResult<User> findUserById(
-            @ApiParam(value = "회원 고유 아이디", required = true) @PathVariable Long id) {
-        return userService.findByUserId(id);
-    }
 }
