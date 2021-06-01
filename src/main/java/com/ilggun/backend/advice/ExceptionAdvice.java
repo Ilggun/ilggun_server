4063f146f9f03a7e88af52a4b9e8896b498ce607
Dev-Beom <rtw_master@naver.com>
2021-06-02 02:16:18 +0900

    Update Exception Logic
    예외 처리 로직을 업데이트 했습니다.
    권한 별 접근, 로그인 오류

--- src/main/java/com/ilggun/backend/advice/ExceptionAdvice.java
+++ src/main/java/com/ilggun/backend/advice/ExceptionAdvice.java
@@ -1,13 +1,16 @@
 package com.ilggun.backend.advice;
 
+import com.ilggun.backend.advice.exception.CAuthenticationEntryPointException;
 import com.ilggun.backend.advice.exception.CProductNotFoundException;
 import com.ilggun.backend.advice.exception.CUserNotFoundException;
+import com.ilggun.backend.advice.exception.CUsernameSigninFailedException;
 import com.ilggun.backend.response.CommonResult;
 import com.ilggun.backend.response.ResponseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,14 +26,12 @@ public class ExceptionAdvice {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     protected CommonResult defaultException(HttpServletRequest request, Exception e) {
-        // 예외 처리의 메시지를 MessageSource에서 가져오도록 수정
         return responseService.getFailResult(Integer.valueOf(getMessage("unKnown.code")), getMessage("unKnown.msg"));
     }
 
     @ExceptionHandler(CUserNotFoundException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     protected CommonResult userNotFoundException(HttpServletRequest request, CUserNotFoundException e) {
-        // 예외 처리의 메시지를 MessageSource에서 가져오도록 수정
         return responseService.getFailResult(Integer.valueOf(getMessage("userNotFound.code")), getMessage("userNotFound.msg"));
     }
 
@@ -38,6 +39,24 @@ public class ExceptionAdvice {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     protected CommonResult productNotFoundException(HttpServletRequest request, CProductNotFoundException e) {
         return responseService.getFailResult(Integer.valueOf(getMessage("productNotFound.code")), getMessage("productNotFound.msg"));
+    }
+
+    @ExceptionHandler(CAuthenticationEntryPointException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public CommonResult authenticationEntryPointException(HttpServletRequest request, CAuthenticationEntryPointException e) {
+        return responseService.getFailResult(Integer.valueOf(getMessage("entryPointException.code")), getMessage("entryPointException.msg"));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public CommonResult AccessDeniedException(HttpServletRequest request, AccessDeniedException e) {
+        return responseService.getFailResult(Integer.valueOf(getMessage("accessDenied.code")), getMessage("accessDenied.msg"));
+    }
+
+
+    @ExceptionHandler(CUsernameSigninFailedException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected CommonResult usernameSigninFailed(HttpServletRequest request, CUsernameSigninFailedException e) {
+        return responseService.getFailResult(Integer.valueOf(getMessage("usernameSigninFailed.code")), getMessage("usernameSigninFailed.msg"));
     }
 
     // code정보에 해당하는 메시지를 조회합니다.
---
+++ src/main/java/com/ilggun/backend/advice/exception/ExceptionController.java
@@ -0,0 +1,24 @@
+package com.ilggun.backend.advice.exception;
+
+import com.ilggun.backend.response.CommonResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/exception")
+public class ExceptionController {
+
+    @GetMapping(value = "/entrypoint")
+    public CommonResult entrypointException() {
+        throw new CAuthenticationEntryPointException();
+    }
+
+    @GetMapping(value = "/accessDenied")
+    public CommonResult accessDeniedException() {
+        throw new AccessDeniedException("");
+    }
+}
