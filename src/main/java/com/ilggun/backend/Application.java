b38b72dfbaf1e871b93b5a6924c3eef97433d1b3
Dev-Beom <youngbeom0908@gmail.com>
2021-06-13 00:40:34 +0900

    Update Application.java

--- src/main/java/com/ilggun/backend/Application.java
+++ src/main/java/com/ilggun/backend/Application.java
@@ -20,4 +20,10 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
